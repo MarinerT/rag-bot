8a06c8382eed049e8289adc8790c1ea73cff74d5
Todd Marino <marino.todd@gmail.com>
2024-03-08 21:06:33 -0500

    linted and tox.ini added

--- app.py
+++ app.py
@@ -1,54 +1,73 @@
+import openai
 import panel as pn
 import param
-import openai
+
 from chatbot import cbfs, unzip_file
 
 
 def initialize_api_dependent_components(api_key, pdf_doc):
     openai.api_key = api_key
     cb = cbfs(pdf_doc=pdf_doc)
-    file_input = pn.widgets.FileInput(accept='.pdf')
-    button_load = pn.widgets.Button(name="Load DB", button_type='primary')
-    button_clearhistory = pn.widgets.Button(name="Clear History", button_type='warning')
+    file_input = pn.widgets.FileInput(accept=".pdf")
+    button_load = pn.widgets.Button(name="Load DB", button_type="primary")
+    button_clearhistory = pn.widgets.Button(
+        name="Clear History", button_type="warning")
     button_clearhistory.on_click(cb.clr_history)
-    inp = pn.widgets.TextInput( placeholder='Enter text here…')
+    inp = pn.widgets.TextInput(placeholder="Enter text here…")
     button_submit = pn.widgets.Button(name="Submit")
     bound_button_load = pn.bind(cb.call_load_db, button_load.param.clicks)
-    conversation = pn.bind(cb.convchain, inp) 
+    conversation = pn.bind(cb.convchain, inp)
 
-    jpg_pane = pn.pane.Image( './img/convchain.jpg')
+    jpg_pane = pn.pane.Image("./img/convchain.jpg")
 
     tab1 = pn.Column(
         pn.Row(inp, button_submit),
         pn.layout.Divider(),
-        pn.panel(conversation,  loading_indicator=True, height=300),
+        pn.panel(conversation, loading_indicator=True, height=300),
         pn.layout.Divider(),
     )
-    tab2= pn.Column(
+    tab2 = pn.Column(
         pn.panel(cb.get_lquest),
         pn.layout.Divider(),
-        pn.panel(cb.get_sources ),
+        pn.panel(cb.get_sources),
     )
-    tab3= pn.Column(
+    tab3 = pn.Column(
         pn.panel(cb.get_chats),
         pn.layout.Divider(),
     )
-    tab4=pn.Column(
-        pn.Row( file_input, button_load, bound_button_load),
-        pn.Row( button_clearhistory, pn.pane.Markdown("Clears chat history. Can use to start a new topic" )),
+    tab4 = pn.Column(
+        pn.Row(file_input, button_load, bound_button_load),
+        pn.Row(
+            button_clearhistory,
+            pn.pane.Markdown(
+                """
+                                Clears chat history.
+                                Can use to start a new topic"""
+            ),
+        ),
         pn.layout.Divider(),
-        pn.Row(jpg_pane.clone(width=400))
+        pn.Row(jpg_pane.clone(width=400)),
     )
     dashboard = pn.Column(
-        pn.Row(pn.pane.Markdown('# ChatWithYourData_Bot')),
-        pn.Tabs(('Conversation', tab1), ('Database', tab2), ('Chat History', tab3),('Configure', tab4))
+        pn.Row(pn.pane.Markdown("# ChatWithYourData_Bot")),
+        pn.Tabs(
+            ("Conversation", tab1),
+            ("Database", tab2),
+            ("Chat History", tab3),
+            ("Configure", tab4),
+        ),
     )
+
     return dashboard
 
+
 class Application(param.Parameterized):
-    api_key_input = pn.widgets.PasswordInput(name='API Key', placeholder='Enter your API key here')
-    submit_button = pn.widgets.Button(name='Submit API Key', button_type='primary')
-    status_message = pn.pane.Markdown('Enter API Key to unlock full features.')
+    api_key_input = pn.widgets.PasswordInput(
+        name="API Key", placeholder="Enter your API key here"
+    )
+    submit_button = pn.widgets.Button(
+        name="Submit API Key", button_type="primary")
+    status_message = pn.pane.Markdown("Enter API Key to unlock full features.")
     main_container = pn.Column()  # Container for dynamic content
 
     def __init__(self, **params):
@@ -60,32 +79,40 @@ class Application(param.Parameterized):
 
     def submit_api_key(self, event):
         api_key = self.api_key_input.value
-        acknowledgment_area = pn.pane.Markdown('')
+        acknowledgment_area = pn.pane.Markdown("")
         # Write API key to .env
         if api_key:
-            with open('.env', 'a') as env_file:
-                env_file.write(f'OPENAI_API_KEY={api_key}\n')
+            with open(".env", "a") as env_file:
+                env_file.write(f"OPENAI_API_KEY={api_key}\n")
 
             # Display a message to confirm the API key has been stored
             print("API key has been successfully stored in the .env file.")
 
             # Adds the new folder "resume" to gitignore
-            print("""add the folders resume and docs/chroma to gitignore if it exists
-                or it creates a .gitignore if it doesn't exist in this directory.""")
+            print(
+                """
+                  add the folders resume and docs/chroma
+            to gitignore if it exists or it creates a .gitignore
+            if it doesn't exist in this directory.
+                  """
+            )
             with open(".gitignore", "a") as g:
                 g.write("\nresume/")
                 g.write("\ndocs/chroma/")
             print("folders added to gitignore and possibly created .gitignore")
             pdf_doc = unzip_file("resume.pdf.zip", "resume")
-            dashboard = initialize_api_dependent_components(api_key, pdf_doc)  # This should return the dashboard component
+            dashboard = initialize_api_dependent_components(api_key, pdf_doc)
             self.main_container.clear()  # Clear existing content
             self.main_container.append(dashboard)  # Add the new dashboard
 
             # Clear the input and update acknowledgment
-            self.api_key_input.value = ''
-            acknowledgment_area.object = "API key successfully added to .env file."
+            self.api_key_input.value = ""
+            acknowledgment_area.object = """
+            API key successfully added to .env file.
+            """
         else:
-            self.status_message.object = 'Invalid API Key.'
+            self.status_message.object = "Invalid API Key."
+
 
 # Initialize your application
 app_layout = Application()
--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,16 @@
 import os
-import panel as pn
-import param
 import zipfile
 
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import DocArrayInMemorySearch
+import panel as pn
+import param
+from dotenv import find_dotenv, load_dotenv
 from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
+_ = load_dotenv(find_dotenv())  # read local .env file
 
 llm_name = "gpt-4"
 chunk_size = 126
@@ -18,7 +18,7 @@ chunk_overlap = 12
 
 
 def unzip_file(zip_file, destination):
-    with zipfile.ZipFile(zip_file, 'r') as ref:
+    with zipfile.ZipFile(zip_file, "r") as ref:
         ref.extractall(destination)
     return os.path.join(destination, "resume.pdf")
 
@@ -28,93 +28,134 @@ def load_db(file, chain_type, k):
     loader = PyPDFLoader(file)
     documents = loader.load()
     # split documents
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    )
     docs = text_splitter.split_documents(documents)
     # define embedding
     embeddings = OpenAIEmbeddings()
     # create vector database from data
     db = DocArrayInMemorySearch.from_documents(docs, embeddings)
     # define retriever
-    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": k})
+    retriever = db.as_retriever(
+        search_type="similarity",
+        search_kwargs={
+            "k": k})
     # create a chatbot chain. Memory is managed externally.
     qa = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(model_name=llm_name, temperature=0), 
-        chain_type=chain_type, 
-        retriever=retriever, 
+        llm=ChatOpenAI(model_name=llm_name, temperature=0),
+        chain_type=chain_type,
+        retriever=retriever,
         return_source_documents=True,
         return_generated_question=True,
     )
-    return qa 
+    return qa
+
 
 class cbfs(param.Parameterized):
     chat_history = param.List([])
     answer = param.String("")
-    db_query  = param.String("")
+    db_query = param.String("")
     db_response = param.List([])
-    
+
     def __init__(self, pdf_doc, **params):
-        super(cbfs, self).__init__( **params)
+        super(cbfs, self).__init__(**params)
         self.panels = []
         self.loaded_file = pdf_doc
-        self.qa = load_db(self.loaded_file,"stuff", 10)
-    
+        self.qa = load_db(self.loaded_file, "stuff", 10)
+
     def call_load_db(self, count):
-        if count == 0 or file_input.value is None:  # init or no file specified :
+        if count == 0 or self.file_input.value is None:
             return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
         else:
-            file_input.save("temp.pdf")  # local copy
-            self.loaded_file = file_input.filename
-            button_load.button_style="outline"
+            self.file_input.save("temp.pdf")  # local copy
+            self.loaded_file = self.file_input.filename
+            self.button_load.button_style = "outline"
             self.qa = load_db("temp.pdf", "stuff", 4)
-            button_load.button_style="solid"
+            self.button_load.button_style = "solid"
         self.clr_history()
         return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
 
     def convchain(self, query):
         if not query:
-            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
-        result = self.qa.invoke({"question": query, "chat_history": self.chat_history})
+            return pn.WidgetBox(
+                pn.Row("User:", pn.pane.Markdown("", width=600)), scroll=True
+            )
+        result = self.qa.invoke(
+            {"question": query, "chat_history": self.chat_history})
         self.chat_history.extend([(query, result["answer"])])
         self.db_query = result["generated_question"]
         self.db_response = result["source_documents"]
-        self.answer = result['answer'] 
-        self.panels.extend([
-            pn.Row('User:', pn.pane.Markdown(query, width=600)),
-            pn.Row('ChatBot:', pn.pane.Markdown(self.answer, width=600, styles={'background-color': '#F6F6F6'}))
-        ])
-         # inp.value = ''  #clears loading indicator when cleared
-        return pn.WidgetBox(*self.panels,scroll=True)
+        self.answer = result["answer"]
+        self.panels.extend(
+            [
+                pn.Row(
+                    "User:", pn.pane.Markdown(
+                        query, width=600)), pn.Row(
+                    "ChatBot:", pn.pane.Markdown(
+                        self.answer, width=600, styles={
+                            "background-color": "#F6F6F6"}), ), ])
+        # inp.value = ''  #clears loading indicator when cleared
+        return pn.WidgetBox(*self.panels, scroll=True)
 
-    @param.depends('db_query ', )
+    @param.depends(
+        "db_query ",
+    )
     def get_lquest(self):
         if not self.db_query:
             return pn.Column(
-                pn.Row(pn.pane.Markdown(f"Last question to DB:", styles={'background-color': '#F6F6F6'})),
-                pn.Row(pn.pane.Str("no DB accesses so far"))
+                pn.Row(
+                    pn.pane.Markdown(
+                        "Last question to DB:",
+                        styles={
+                            "background-color": "#F6F6F6"})),
+                pn.Row(
+                    pn.pane.Str("no DB accesses so far")),
             )
         return pn.Column(
-            pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color': '#F6F6F6'})),
-            pn.pane.Str(self.db_query )
+            pn.Row(
+                pn.pane.Markdown(
+                    "DB query:",
+                    styles={
+                        "background-color": "#F6F6F6"})),
+            pn.pane.Str(
+                self.db_query),
         )
 
-    @param.depends('db_response', )
+    @param.depends(
+        "db_response",
+    )
     def get_sources(self):
         if not self.db_response:
-            return 
-        rlist=[pn.Row(pn.pane.Markdown(f"Result of DB lookup:", styles={'background-color': '#F6F6F6'}))]
+            return
+        rlist = [
+            pn.Row(
+                pn.pane.Markdown(
+                    "Result of DB lookup:",
+                    styles={
+                        "background-color": "#F6F6F6"}))]
         for doc in self.db_response:
             rlist.append(pn.Row(pn.pane.Str(doc)))
         return pn.WidgetBox(*rlist, width=600, scroll=True)
 
-    @param.depends('convchain', 'clr_history') 
+    @param.depends("convchain", "clr_history")
     def get_chats(self):
         if not self.chat_history:
-            return pn.WidgetBox(pn.Row(pn.pane.Str("No History Yet")), width=600, scroll=True)
-        rlist=[pn.Row(pn.pane.Markdown(f"Current Chat History variable", styles={'background-color': '#F6F6F6'}))]
+            return pn.WidgetBox(
+                pn.Row(pn.pane.Str("No History Yet")), width=600, scroll=True
+            )
+        rlist = [
+            pn.Row(
+                pn.pane.Markdown(
+                    "Current Chat History variable",
+                    styles={"background-color": "#F6F6F6"},
+                )
+            )
+        ]
         for exchange in self.chat_history:
             rlist.append(pn.Row(pn.pane.Str(exchange)))
         return pn.WidgetBox(*rlist, width=600, scroll=True)
 
-    def clr_history(self,count=0):
+    def clr_history(self, count=0):
         self.chat_history = []
-        return 
+        return
